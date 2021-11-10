0a76e226ab6b7aed1763ce9f408e25b8759423fc
Mogeko <zhengjunyi@live.com>
2021-11-10 13:14:21 +0000

    build: creat setup.py to set up project

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+name = "video_creator"
+version = "0.0.1"
+
+setup(
+    name=name,
+    version=version,
+    author="",
+    author_email="",
+    description="Use Python to multiple videos in one and simple syncronization. ",
+    url="https://github.com/SDM-2021-16-SpongeBob/video_creator",
+    packages=find_packages(),
+
+    install_requires=[],
+
+    tests_require=[],
+
+    classifiers=[],
+)
