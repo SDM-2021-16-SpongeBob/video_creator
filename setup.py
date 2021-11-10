941e94c958bd4ed7af1ff2a7639d295b5f22b812
Mogeko <zhengjunyi@live.com>
2021-11-10 13:22:57 +0000

    build: set authors in setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ version = "0.0.1"
 setup(
     name=name,
     version=version,
-    author="",
-    author_email="",
+    author="mogeko kumarramesh454 cupofocha Morristt chesionyaya Mashhood555",
     description="Use Python to multiple videos in one and simple syncronization. ",
     url="https://github.com/SDM-2021-16-SpongeBob/video_creator",
     packages=find_packages(),
