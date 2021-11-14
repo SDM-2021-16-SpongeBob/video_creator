7be30e85770200386eb36e3d54f2541564518050
Mogeko <zhengjunyi@live.com>
2021-11-14 15:49:30 +0000

    feat: handle videos

--- video_creator/__main__.py
+++ video_creator/__main__.py
@@ -3,6 +3,7 @@ Inlet of the program
 """
 
 import argparse
+from video_creator.lib import InputVideo, OutputVideo
 
 
 def main():
@@ -10,11 +11,16 @@ def main():
 
     parser = argparse.ArgumentParser(
         description='Multiple videos in one and simple syncronization.')
-    parser.add_argument('out_file', metavar='Output', type=str,
+    parser.add_argument('out_file', metavar='Output File', type=str,
                         nargs=1, help='The result of the synthetic video')
-    parser.add_argument('--input', nargs=2,
+    parser.add_argument('-i', '--input', nargs=2,
                         help='Entered video files')
     args = parser.parse_args()
+
+    input_files: list = args['input']
+    output_file: str = args['out_file']
+
+    OutputVideo().input(InputVideo(input_files[0])).input(InputVideo(input_files[1])).gen_outputfile(output_file).handle().close()
 
 
 if __name__ == '__main__':
