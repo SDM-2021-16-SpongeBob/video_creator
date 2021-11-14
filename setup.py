a57269ed6e75c70c5488093471413c02fcdff6f4
chesionyaya <83960173+chesionyaya@users.noreply.github.com>
2021-11-14 22:08:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,11 +24,5 @@ setup(
         ]
     },
 
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: End Users/Desktop',
-        'Topic :: Software Development',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.9',
-    ],
+  
 )
