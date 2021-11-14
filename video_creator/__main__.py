a66cd902cd7b1fe26903fabfd8187d47948d7b44
Mogeko <zhengjunyi@live.com>
2021-11-14 15:36:56 +0000

    feat: use argparse handle args

--- video_creator/__main__.py
+++ video_creator/__main__.py
@@ -2,10 +2,19 @@
 Inlet of the program
 """
 
+import argparse
+
 
 def main():
     """Main function"""
-    pass
+
+    parser = argparse.ArgumentParser(
+        description='Multiple videos in one and simple syncronization.')
+    parser.add_argument('out_file', metavar='Output', type=str,
+                        nargs=1, help='The result of the synthetic video')
+    parser.add_argument('--input', nargs=2,
+                        help='Entered video files')
+    args = parser.parse_args()
 
 
 if __name__ == '__main__':
